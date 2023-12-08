423dbc69c3a65f339b10184d3a4ab8e8b8875ed1
caroline opiyo <opiyocaroline20@gmail.com>
2023-12-08 21:13:43 +0300

    Rename column in Student model

---
+++ lib/migrations/versions/920b24c98e64_rename_column_in_student_model.py
@@ -0,0 +1,24 @@
+"""Rename column in Student model
+
+Revision ID: 920b24c98e64
+Revises: 791279dd0760
+Create Date: 2023-12-08 21:04:18.770496
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '920b24c98e64'
+down_revision = '791279dd0760'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
